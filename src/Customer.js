b3d15a10433c6f2855d3b7718d67bc689e0f12b5
Lakkhana2079 <lakkhana2079@gmail.com>
2023-09-29 15:16:00 +0700

    update

--- src/Customer.js
+++ src/Customer.js
@@ -1,104 +1,75 @@
+const express = require('express');
+const sqlite3 = require('sqlite3');
+const app = express();
 
+const db = new sqlite3.Database('./Database/Customers.sqlite');
 
-const express = require('express'); 
-const Sequelize = require('sequelize'); 
-const app = express(); 
+app.use(express.json());
 
-// parse incoming requests 
-app.use(express.json()); 
+db.run(`CREATE TABLE IF NOT EXISTS Customers (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    author TEXT
+)`);
 
-// create a connection to the database 
-const sequelize = new Sequelize('database', 'username', 'password', { 
-    host: 'localhost', 
-    dialect: 'sqlite', 
-    storage: './Database/Customer.sqlite' 
+app.get('/Customers', (req, res) => {
+    db.all('SELECT * FROM Customers', (err, rows) => {
+        if(err) {
+            res.status(500).send(err);   
+        }
+        else{
+            res.json(rows);
+        }
+    });
 });
 
-// define the Book model 
-const Book = sequelize.define('book', { 
-    id: { 
-        type: Sequelize. INTEGER, 
-        autoIncrement: true, 
-        primaryKey: true 
-    }, 
-    title: { 
-        type: Sequelize.STRING, 
-        allowNull: false 
-    }, 
-    author: { 
-        type: Sequelize.STRING, 
-        allowNull: false 
+app.get('/Customers/:id', (req, res) => {
+    db.get('SELECT * FROM Customers WHERE id = ?', req.params.id, (err, rows) => {
+        if(err) {
+            res.status(500).send(err);   
+        } else {
+            if (!row) {
+                res.status(404).send('Customer not found');
+            }
+            else {
+                res.json(row)
+            }
+        }
+    });
+});
+
+app.post('/Customers', (req, res) => {
+    const Customer = req.body;
+    db.run('INSERT INTO Customers (title, author) VALUES (?, ?)', Customer.title, Customer.author, function(err) {
+    if (err) {
+        res.status(500).send(err);
+    } else {
+        Customer.id = this.lastID;
+        res.send(Customer); 
     }
+    });   
 });
 
-// create the books table if it doesn't exist 
-sequelize.sync();
-
-// route to get all books 
-app.get('/books', (req, res) => { 
-    Book.findAll().then(books => { 
-        res.json(books); 
-    }).catch(err => { 
-        res.status(500).send(err); 
-    }); 
-}); 
-
-// route to get a book by id 
-app.get('/books/:id', (req, res) => { 
-    Book.findByPk (req.params.id).then(book => { 
-        if (!book) { 
-            res.status(404).send('Book not found'); 
-        } else { 
-            res.json(book); 
-        } 
-    }).catch(err => { 
-        res.status(500).send(err); 
-    }); 
+app.put('/Customers/:id', (req, res) => {
+    const Customer = req.body;
+    db.run('UPDATE Customers SET title = ?, author = ? WHERE id = ?', Customer.title, Customer.author, req.params.id, function(err) {
+    if (err) {
+        res.status(500).send(err);
+    } else {
+        res.send(Customer); 
+    }
+    });   
 });
 
-// route to create a new book 
-app.post('/books', (req, res) => { 
-    Book.create(req.body).then(book => { 
-        res.send(book); 
-    }).catch(err => { 
-        res.status(500).send(err); 
-    }); 
-}); 
-
-// route to update a book 
-app.put('/books/:id', (req, res) => {
-    Book.findByPk (req.params.id).then (book => { 
-        if (!book) { 
-            res.status(404).send('Book not found'); 
-        } else { 
-            book.update(req.body).then(() => { 
-                res.send(book); 
-            }).catch(err => { 
-                res.status(500).send(err); 
-            }); 
-        } 
-    }).catch(err => { 
-        res.status(500).send(err); 
-    }); 
+app.delete('/Customers/:id', (req, res) => {
+    db.run('DELETE FROM Customers WHERE id = ?', req.params.id, function(err) {
+    if (err) {
+        res.status(500).send(err);
+    } else {
+        res.send({}); 
+    }
+    });   
 });
 
-// route to delete a book 
-app.delete('/books/:id', (req, res) => { 
-    Book.findByPk(req.params.id).then(book => { 
-        if (!book) { 
-            res.status(404).send('Book not found'); 
-        } else { 
-            book.destroy().then(() => { 
-                res.send({}); 
-            }).catch(err => { 
-                res.status(500).send(err); 
-            });
-        } 
-    }).catch(err => { 
-        res.status(500).send(err); 
-    }); 
-}); 
-
-// start the server 
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
